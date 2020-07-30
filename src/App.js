211f3bcc291fe0a306121c19707b70cf2e9a12ad
Lazaropower <lazaroesunagranpersona@gmail.com>
2020-07-30 18:28:07 +0200

    Added react base, components structure and skeleton of the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+
+import Navigation from "./components/Navigation"; 
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Item from "./components/Item";
+import Catalogue from "./components/Catalogue";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navigation />
+      <div className="container p-4">
+        <Route path="/" exact component={Catalogue} />
+        <Route path="/item/:id" exact component={Item} />
+        <Route path="/register" exact component={Register} />
+        <Route path="/login" exact component={Login} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+export default class Login extends Component {
+
+    state ={
+        username: '',
+        password: ''
+    }
+
+    render() {
+        return (
+            <p>Login</p>
+        );
+    }
+}
