27e67518632d46222cfcfae0ead7e7400ecae2be
Lazaropower <lazaroesunagranpersona@gmail.com>
2020-07-31 11:37:59 +0200

    Added more styles to the components and example movie

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import {Nav, Navbar, Form, FormControl, Button} from 'react-bootstrap';
 
 export default class Navigation extends Component {
-    render() {
-        return (
-            <div>
-                <p>Navigation bar</p>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <Navbar bg="light" variant="light" expand="lg" sticky="top">
+        <Navbar.Brand href="/"><span role="img" aria-label="moovie">🎬</span> Movies App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+        <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-auto mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+          <Nav className="ml-auto">
+            <Nav.Link href="/login">Login <i class="fa fa-lock"></i></Nav.Link>
+          </Nav>
+         
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,11 +1,56 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Register extends Component {
-    render() {
-        return (
-            <div>
-                <p>Register</p>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="login-form">
+        <div className="card card-body">
+          <h2 className="text-center">Sign up</h2>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Username"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="E-mail"
+                required
+                />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Confirm password"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <button class="btn btn-primary btn-block">Register</button>
+          </div>
+          <Link to="/login">
+            <p className="text-center">Already registered?</p>
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
